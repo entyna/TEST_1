40931b9b605aaa3eca7fbab69c22a05286e6278c
Valentýna Kociánová <123517319+entyna@users.noreply.github.com>
2023-04-30 11:59:44 +0200

    webkit fill

--- sketch.js
+++ sketch.js
@@ -5,12 +5,12 @@ let dur = 2000; // in milliseconds
 let pg;
 
 let points = [
-   [0, 0],
-   [0, 1],
-   [0, 2],
-   [0, 3],
-   [0, 4],
-   [0, 5]
+   [1, 0],
+   [1, 1],
+   [1, 2],
+   [1, 3],
+   [1, 4],
+   [1, 5]
 ];
 
 function updatePoints(yaoValues) {
